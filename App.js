1f9aeaf8f5ae526e4b9db725d4e806e43f3d6f6f
Mayank Jain <jainmayank16041999@gmail.com>
2020-11-23 00:07:22 +0530

    custom picker created

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
-// import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { View, Text } from 'react-native';
-
-
-import AppText from './app/components/AppText';
-import { MaterialCommunityIcons } from '@expo/vector-icons'
-import AppButton from './app/components/AppButton';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
+import React, { useState } from 'react';
+import { Switch, Text, TextInput } from 'react-native';
+import AppPicker from './app/components/AppPicker';
+import AppTextInput from './app/components/AppTextInput';
 import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
-import AccountsScreen from './app/screens/AccountsScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
 
+const categories = [
+  {
+    label: 'Furniture',
+    value: 1
+  },
+  {
+    label: 'Clothing',
+    value: 2
+  },
+  {
+    label: 'Camera',
+    value: 3
+  },
+]
 
 export default function App() {
+
+  const [category, setCategory] = useState(categories[0])
+
   return (
-    <ListingsScreen />
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={item => setCategory(item)}
+        items={categories} icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
   );
 }
 
