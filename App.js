d8a9a1c24eb1edfb12660999366c79f80f959931
Mayank Jain <jainmayank16041999@gmail.com>
2020-11-24 12:29:52 +0530

    login screen form completed

--- App.js
+++ App.js
@@ -1,36 +1,11 @@
 import React, { useState } from 'react';
-import { Switch, Text, TextInput } from 'react-native';
-import AppPicker from './app/components/AppPicker';
-import AppTextInput from './app/components/AppTextInput';
-import Screen from './app/components/Screen';
 
-const categories = [
-  {
-    label: 'Furniture',
-    value: 1
-  },
-  {
-    label: 'Clothing',
-    value: 2
-  },
-  {
-    label: 'Camera',
-    value: 3
-  },
-]
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
 
-  const [category, setCategory] = useState(categories[0])
-
   return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={item => setCategory(item)}
-        items={categories} icon="apps" placeholder="Category" />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
+    <LoginScreen />
   );
 }
 
