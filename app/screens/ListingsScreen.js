642dff0632610a0a2db30240555a24bf5caad856
Mayank Jain <jainmayank16041999@gmail.com>
2020-11-26 19:15:00 +0530

    connected backend, added loading animation

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,31 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FlatList, StyleSheet } from 'react-native'
 
+import Button from '../components/Button'
 import Card from '../components/Card'
 import Screen from '../components/Screen'
 import colors from '../config/colors'
 import routes from '../navigation/routes'
 
-const listings = [
-    {
-        id: 1,
-        title: 'Red Jacket',
-        price: 100,
-        image: require("../assets/jacket.jpg")
+import ActivityIndicator from '../components/ActivityIndicator'
 
-    },
-    {
-        id: 2,
-        title: 'Couch',
-        price: 1000,
-        image: require("../assets/couch.jpg")
+import listingApi from '../api/listings'
+import AppText from '../components/Text'
 
-    }
-]
 
 export default function ListingsScreen({ navigation }) {
+
+    const [listings, setListings] = useState([])
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        loadListings()
+    }, [])
+
+    const loadListings = async () => {
+        setLoading(true)
+        const response = await listingApi.getListings()
+        setLoading(false)
+
+        if (!response.ok) return setError(true)
+
+        setError(false)
+        setListings(response.data)
+    }
+
     return (
         <Screen style={styles.screen}>
+            {
+                error &&
+                <React.Fragment>
+                    <AppText>Couldn't retrieve the listings</AppText>
+
+                    <Button title="Retry" onPress={loadListings} />
+                </React.Fragment>
+            }
+            <ActivityIndicator visible={loading} />
+
             <FlatList
                 data={listings}
                 keyExtractor={listings => listings.id.toString()}
@@ -33,11 +53,12 @@ export default function ListingsScreen({ navigation }) {
                     <Card
                         title={item.title}
                         subTitle={"$" + item.price}
-                        image={item.image}
+                        imageUrl={item.images[0].url}
                         onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
                     />
                 }
             />
+
         </Screen>
     )
 }
